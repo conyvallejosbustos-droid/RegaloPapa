83f2dd4ac56ea680f5f559d0ca4e9483c1fc7c97
conyvallejosbustos-droid <conyvallejosbustos@gmail.com>
2026-06-19 14:52:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mensaje(){
+
+document.getElementById("mensaje").innerHTML =
+"Gracias papá por esforzarte todos los días por nosotras y por enseñarme a ser perseverante en la vida <br><br>Eres mi ídolo y mi ejemplo a seguir.<br><br> Te amo mucho, feliz día del padre <3";
+
+}
