7f02036a1055e49b190bc2c456554ad1749ebaa5
conyvallejosbustos-droid <conyvallejosbustos@gmail.com>
2026-06-21 16:35:36 -0400

    Refactor abrirCarta function to include photo display

--- script.js
+++ script.js
@@ -11,11 +11,14 @@ document.getElementById("mensaje").innerHTML =
 
 "Te amo mucho pa, yo veo como todos los días desde que te levantas hasta que duermes haces de todo para que estemos bien, no se si te lo había dicho antes pero te admiro demasiado y te considero como mi ídolo, eres la persona mas inteligente que conozco y toda tu trayectoria de vida me parece como épica, como cuando aprendiste a programar solo y le hiciste la broma a tus vecinos, como incluso con muchos cambios de colegio e inestabilidad siempre fuiste de los mejores por no decir el mejor de la clase, como tocas la guitarra me parece bakan, toda tu vida universitaria y las cosas que me cunetas me dejan pa la caga porque me hacen dar cuenta lo pillo y capo que eres creo que básicamente eso es lo que admiro tanto de de ti.<br><br>" +
 
-"También te quiero decir que por favor te cuides y que intentes tener una buena salud por que sé que nada es eterno y que algún día lo voy a perder, pero quiero que eso sea en muuucho tiempo más porque aún necesito tu guía, apoyo y cariño (aunque no se si en algún momento la deje de necesitar)." +
+"También te quiero decir que por favor te cuides y que intentes tener una buena salud por que sé que nada es eterno y que algún día lo voy a perder, pero quiero que eso sea en muuucho tiempo más porque aún necesito tu guía, apoyo y cariño (aunque no se si en algún momento la deje de necesitar).<br><br>" +
 
 "De verdad me da mucha pena saber que talvez desde el próximo año lo voy a ver muy poco porque usted significa mucho para mi pero voy a intentar ser mas como tu y no hacer rabiar a la mamá ni pelearme tanto con la Cami.<br><br>" +
 
-"Gracias totales por ser como es y por ser mi ídolo, maestro, bufón, cómplice, guía y mi papá.";
+"Gracias totales por ser como es y por ser mi ídolo, maestro, bufón, cómplice, guía y mi papá. ❤️";
+
+document.getElementById("foto").style.display = "block";
+
 
 document.getElementById("boton").style.display="none";
 
