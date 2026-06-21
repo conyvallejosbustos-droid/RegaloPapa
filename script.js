15d8fb276c987440efaa62687b41e5a1860bc218
conyvallejosbustos-droid <conyvallejosbustos@gmail.com>
2026-06-21 16:14:58 -0400

    Replace mensaje function with abrirCarta function

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function mensaje(){
+function abrirCarta(){
+
+document.getElementById("sobre").classList.add("abierto");
+
+document.getElementById("tapa").classList.add("abrir");
+
 
 document.getElementById("mensaje").innerHTML =
-"Gracias papá por esforzarte todos los días por nosotras y por enseñarme a ser perseverante en la vida <br><br>Eres mi ídolo y mi ejemplo a seguir.<br><br> Te amo mucho, feliz día del padre <3";
+
+"FELIZ DIA DEL PADRE! <br><br>" +
+
+"Te amo mucho pa, yo veo como todos los días desde que te levantas hasta que duermes haces de todo para que estemos bien, no se si te lo había dicho antes pero te admiro demasiado y te considero como mi ídolo, eres la persona mas inteligente que conozco y toda tu trayectoria de vida me parece como épica, como cuando aprendiste a programar solo y le hiciste la broma a tus vecinos, como incluso con muchos cambios de colegio e inestabilidad siempre fuiste de los mejores por no decir el mejor de la clase, como tocas la guitarra me parece bakan, toda tu vida universitaria y las cosas que me cunetas me dejan pa la caga porque me hacen dar cuenta lo pillo y capo que eres creo que básicamente eso es lo que admiro tanto de de ti.<br><br>" +
+
+"También te quiero decir que por favor te cuides y que intentes tener una buena salud por que sé que nada es eterno y que algún día lo voy a perder, pero quiero que eso sea en muuucho tiempo más porque aún necesito tu guía, apoyo y cariño (aunque no se si en algún momento la deje de necesitar)." +
+
+"De verdad me da mucha pena saber que talvez desde el próximo año lo voy a ver muy poco porque usted significa mucho para mi pero voy a intentar ser mas como tu y no hacer rabiar a la mamá ni pelearme tanto con la Cami.<br><br>" +
+
+"Gracias totales por ser como es y por ser mi ídolo, maestro, bufón, cómplice, guía y mi papá.";
+
+document.getElementById("boton").style.display="none";
 
 }
